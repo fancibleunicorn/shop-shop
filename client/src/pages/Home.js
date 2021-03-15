155c098a485a8a47dcc7ec437bd377b4d3479ec1
Adam Crandall <adamcrandall91@gmail.com>
2021-03-15 16:33:50 -0400

    finished 22.1

---
+++ client/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ProductList from "../components/ProductList";
+import CategoryMenu from "../components/CategoryMenu";
+
+const Home = () => {
+  // no longer needs state
+  // const [currentCategory, setCategory] = useState("");
+
+  return (
+    <div className="container">
+      <CategoryMenu />
+      <ProductList />
+    </div>
+  );
+};
+
+export default Home;
