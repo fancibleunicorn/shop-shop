c6d354914a5312fefd388a19d1d84e83093a00bc
Adam Crandall <adamcrandall91@gmail.com>
2021-03-16 15:58:30 -0400

    finished 22.2

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
+import Cart from '../components/Cart';
 
 const Home = () => {
   // no longer needs state
@@ -10,6 +11,7 @@ const Home = () => {
     <div className="container">
       <CategoryMenu />
       <ProductList />
+      <Cart />
     </div>
   );
 };
